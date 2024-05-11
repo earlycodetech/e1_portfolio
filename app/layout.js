e992bb20bb56eb2c955a423bdfa68b6e9e4d9319
emma2625 <emmanuelodobo10@gmail.com>
2024-05-11 12:32:03 +0100

    Class Update

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"] , weight: ['100', '600', '900'] });
 
 export const metadata = {
   title: "My Portfolio - Delivering Solutions",
   description: "Hi, my name is Earlycode and I am a web developer",
   favicon: "/logo.png"
 };
-
+// TODO: Check favicon
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
