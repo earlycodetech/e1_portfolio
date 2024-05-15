5f5bd7613af5bba129bcb3a46b034fd09bd9fcb1
emma2625 <emmanuelodobo10@gmail.com>
2024-05-15 14:52:20 +0100

    Update

--- app/(guest)/page.jsx
+++ app/(guest)/page.jsx
@@ -1,23 +1,40 @@
 import React from "react";
-import "../../styles/index.css"
+import "../../styles/index.css";
 import Image from "next/image";
 const HomePage = () => {
   return (
     <main>
       <div className="bg-black h-[32rem] flex items-center gap-5 justify-center hero">
-          <div className="w-full">
+        <div className="w-full pl-10 leading-8">
+          <p className="font-semibold">Get The Best Web Solutions</p>
 
-          </div>
+          <p className="text-5xl font-bold">Hi, I'm Earlycode</p>
 
-          <div className="h-full w-full"> 
-            <Image 
-              src={'/user.png'}
-              alt="User"
-              width={1800}
-              height={900}
-              className="w-full h-full object-contain"
-            />
-          </div>
+          <p className="font-semibold">
+            I am a full stack web developer, delivering web solutions using
+            HTML, CSS, JS, React and Next.Js
+          </p>
+        </div>
+
+        <div className="h-full w-full">
+          <Image
+            src={"/user.png"}
+            alt="User"
+            width={1800}
+            height={900}
+            className="w-full h-full object-contain"
+          />
+        </div>
+      </div>
+
+
+      <div className="mt-10">
+        <h3 className="text-center font-bold text-2xl"> My Recent Projects </h3>
+
+
+        <div className="grid lg:grid-cols-3 md:grid-cols-2">
+          
+        </div>
       </div>
     </main>
   );
