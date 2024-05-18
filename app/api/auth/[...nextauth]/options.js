4257c0d68417bb9d27ed2b56511d3495d2e2e4e2
emma2625 <emmanuelodobo10@gmail.com>
2024-05-18 13:18:11 +0100

    Update

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,14 +1,35 @@
-import GoogleProvider from "next-auth/providers/google"
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT,
-            clientSecret: process.env.GOOGLE_SECRET
-        }),
-    ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT,
+      clientSecret: process.env.GOOGLE_SECRET,
+      profile(profile){
+        return {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email,
+            image: profile.picture,
+            address:null,
+            dob: null,
+            phoneNumber: null,
+            role: 'user',
+        }
+      }
+    }),
+  ],
 
-    pages: {
-        signIn: "/signin"
-    }
-}
+  pages: {
+    signIn: "/signin",
+  },
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY,
+    }),
+  }),
+};
